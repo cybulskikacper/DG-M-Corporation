db3318ad937b9fd89ab11ab8fffd2fea2b38ab22
Kacper <kacpercybulski@icloud.com>
2024-07-16 20:51:16 +0200

    fixed nav

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
+const navLinks = document.querySelectorAll('.nav__link')
+const body = document.body
 const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+
+	if (navMobile.classList.contains('nav-mobile--active')) {
+		body.style.overflow = 'hidden'
+	} else {
+		body.style.overflow = 'auto'
+	}
 }
 
 navBtn.addEventListener('click', handleNav)
+
+const handleNavLinkClick = () => {
+	navBtn.classList.remove('is-active')
+	navMobile.classList.remove('nav-mobile--active')
+	body.style.overflow = 'auto'
+}
+
+// Add click event listener to each navigation link
+navLinks.forEach(link => {
+	link.addEventListener('click', handleNavLinkClick)
+})
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
